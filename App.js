12b775b274f9519695c04f2ce0760051f7b44ccd
mat3ed18 <mathiaspedro9@gmail.com>
2021-10-04 22:25:55 +0000

    adicionando bootstrap + table na view

--- App.js
+++ App.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Table } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
 
     return (
-        <View style={styles.container}>Abc</View>
+        <Table striped={true}>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Username</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td colSpan="2">Larry the Bird</td>
+                    <td>ds</td>
+                </tr>
+            </tbody>
+        </Table>
     );
 }
 
