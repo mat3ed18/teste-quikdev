8d780e328e25cff0f8d10ed227d218f934165ede
mat3ed18 <mathiaspedro9@gmail.com>
2021-10-06 01:15:04 +0000

    organizando o código

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Carousel } from "react-bootstrap";
+import { Carousel, Card, CardGroup } from "react-bootstrap";
 import { Col, Container, Row } from "react-bootstrap";
 import { Modal, Nav, NavItem, NavDropdown } from "react-bootstrap";
 import axios, { AxiosInstance, AxiosPromise } from 'axios';
@@ -13,9 +13,19 @@ export default function App() {
     // useEffect -> document onload page
     const [show, setShow] = useState(false);
 
+    // Definindo as variáveis que serão utilizadas no projeto
+
     const [categorias] = useState([]);
     const [categoriasFilm] = useState([]);
+
+    // Carroséis dos filmes
+
     const [lancamentos] = useState([]);
+    const [novidades] = useState([]);
+    const [destaques] = useState([]);
+
+    // Modal do filme
+
     const [modalMovie, setModalMovie] = useState([]);
 
     const handleClose = () => setShow(false);
@@ -33,37 +43,42 @@ export default function App() {
             const dt_lancamento = new Date(filme["release_date"]);
             const categoriasFilm = [];
 
+            // Preenchendo a lista de categorias/gêneros do filme
+
             filme.genres.forEach((categoria) => {
                 categoriasFilm.push([
                     <button className="btn btn-sm btn-dark">{categoria.name}</button>
                 ]);
-            })
+            });
+
+            // Retornando os dados do filme no Modal
         
             setModalMovie([
                 <><img className="img_modal" src={"https://image.tmdb.org/t/p/original/" + filme["poster_path"]}/>
                 <hr/>
-                <h3>{filme["original_title"]}</h3>
-                <h6><sup>Duração: {filme["runtime"]} minutos</sup></h6>
-                <h6>{filme["popularity"]} visualizações</h6>
-                <h4>{filme["vote_average"]} estrelas</h4>
+                    <h3>{filme["original_title"]}</h3>
+                    <h6><sup>Duração: {filme["runtime"]} minutos</sup></h6>
+                    <h6>{filme["popularity"]} visualizações</h6>
+                    <h4>{filme["vote_average"]} estrelas</h4>
                 <hr/>
-                <h6 className="descricao"><i>{filme["overview"]}</i></h6>
+                    <h6 className="descricao"><i>{filme["overview"]}</i></h6>
                 <hr/>
-                <h6>{filme.production_companies[0].name}</h6>
-                <h6>{filme.production_countries[0].name}</h6>
-                <h6>lançado em {dt_lancamento.getDay()}/{dt_lancamento.getMonth()+1}/{dt_lancamento.getFullYear()}</h6>
-                <h6>idioma: {filme.spoken_languages[0].name}</h6>
+                    <h6>{filme.production_companies[0].name}</h6>
+                    <h6>{filme.production_countries[0].name}</h6>
+                    <h6>lançado em {dt_lancamento.getDay()}/{dt_lancamento.getMonth()+1}/{dt_lancamento.getFullYear()}</h6>
+                    <h6>idioma: {filme.spoken_languages[0].name}</h6>
                 <hr/>
-                <div>
-                    {categoriasFilm}
-                </div></>
+                    <div>
+                        {categoriasFilm}
+                    </div></>
             ]);
         }).catch(function (err) {
+            // Retornando o erro da request
+
             setModalMovie([
                 <i>Erro: Parece que houve uma falha interna em nosso servidor. Tente novamente mais tarde</i>
             ]);
         });
-        alert(id);
     }
 
     useEffect(() => {
@@ -80,7 +95,9 @@ export default function App() {
             document.body.innerHTML = "<i style=\"color: white\">Erro: " + JSON.stringify(err) + "</i>";
         });
 
-        // Carousel 1
+        // Adicionando os filmes nos carroséis
+
+        // Carousel 1: Lançamentos
 
         axios.get("https://api.themoviedb.org/3/discover/movie?api_key=4ec327e462149c3710d63be84b81cf4f&language=pt-BR&page=1").then(function (response) {
             response.data.results.forEach((filme) => {
@@ -93,13 +110,45 @@ export default function App() {
                     </Carousel.Item>
                 );
             });
-        }).catch(function (err) {
-            document.body.innerHTML = "<i style=\"color: white\">Erro: " + JSON.stringify(err) + "</i>";
-        });
+        }).catch(function (err) {  });
+
+        // Carousel 1: Novidades
+
+        axios.get("https://api.themoviedb.org/3/discover/movie?api_key=4ec327e462149c3710d63be84b81cf4f&language=pt-BR&page=2").then(function (response) {
+            response.data.results.forEach((filme) => {
+                novidades.push(
+                    <Carousel.Item onClick={() => openMovie(filme["id"])} style={{backgroundImage: "url('https://image.tmdb.org/t/p/original/" + filme["backdrop_path"] + "')", backgroundRepeat: "no-repeat", backgroundPosition: "center", backgroundSize: "100%"}} >
+                        <img className="d-block" src={"https://image.tmdb.org/t/p/original/" + filme["poster_path"]} />
+                        <Carousel.Caption>
+                            <button className="btn btn-dark openMovie">{filme["original_title"]}</button>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                );
+            });
+        }).catch(function (err) {  });
+
+        // Carousel 1: Destaques
+
+        axios.get("https://api.themoviedb.org/3/discover/movie?api_key=4ec327e462149c3710d63be84b81cf4f&language=pt-BR&page=3").then(function (response) {
+            response.data.results.forEach((filme) => {
+                destaques.push(
+                    <Carousel.Item onClick={() => openMovie(filme["id"])} style={{backgroundImage: "url('https://image.tmdb.org/t/p/original/" + filme["backdrop_path"] + "')", backgroundRepeat: "no-repeat", backgroundPosition: "center", backgroundSize: "100%"}} >
+                        <img className="d-block" src={"https://image.tmdb.org/t/p/original/" + filme["poster_path"]} />
+                        <Carousel.Caption>
+                            <button className="btn btn-dark openMovie">{filme["original_title"]}</button>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                );
+            });
+        }).catch(function (err) {  });
+
+        // Apagando os span dos botões de passar o filme no carrosel
 
         document.querySelectorAll(".sr-only").forEach((item) => { 
             item.remove();
         });
+
+        // Colocando estilo nos botões de passar o filme no carrosel
 
         document.querySelectorAll(".carousel-control-prev").forEach((item) => { 
             item.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
@@ -115,7 +164,8 @@ export default function App() {
 
     return (
 
-            
+        // Modal dos detalhes do filme
+
         <><Modal show={show} onHide={handleClose} size="md">
             <Modal.Header>
                 <Modal.Title>Detalhes do filme</Modal.Title>
@@ -127,6 +177,8 @@ export default function App() {
                 <button className="btn btn-secondary" onClick={handleClose}>Fechar</button>
             </Modal.Footer>
         </Modal>
+
+        {/* Cabeçalho com menu inicial */}
             
         <><><><Nav className="navbar navbar-dark bgDark justify-content-center">
             <Nav.Item>
@@ -140,7 +192,7 @@ export default function App() {
             </NavDropdown>
         </Nav><br/>
 
-        <button className="btn btn-success openModal">Abrir</button>
+        {/* Carrosel dos filmes */}
 
         <h3 className="bgDark text-white p-3">Lançamentos</h3>
 
@@ -151,47 +203,13 @@ export default function App() {
         <h3 className="bgDark text-white p-3">Novidades</h3>
 
         <Carousel variant="dark">
-            <Carousel.Item>
-                <img className="d-block" src="https://via.placeholder.com/100x200.png?text=Filme 1" />
-                <Carousel.Caption class="text-white">
-                    <p>Filme 1</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img className="d-block" src="https://via.placeholder.com/100x200.png?text=Filme 2" />
-                <Carousel.Caption class="text-white">
-                    <p>Filme 2</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img className="d-block" src="https://via.placeholder.com/100x200.png?text=Filme 3" />
-                <Carousel.Caption class="text-white">
-                    <p>Filme 3</p>
-                </Carousel.Caption>
-            </Carousel.Item>
+            {novidades}
         </Carousel></><br />
 
         <h3 className="bgDark text-white p-3">Destaques</h3>
 
         <Carousel variant="dark">
-            <Carousel.Item>
-                <img className="d-block" src="https://via.placeholder.com/100x200.png?text=Filme 1" />
-                <Carousel.Caption class="text-white">
-                    <p>Filme 1</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img className="d-block" src="https://via.placeholder.com/100x200.png?text=Filme 2" />
-                <Carousel.Caption class="text-white">
-                    <p>Filme 2</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img className="d-block" src="https://via.placeholder.com/100x200.png?text=Filme 3" />
-                <Carousel.Caption class="text-white">
-                    <p>Filme 3</p>
-                </Carousel.Caption>
-            </Carousel.Item>
+            {destaques}
         </Carousel></></>
     );
 }
